361630d99862ebead0c1524907d81d17e8ecd204
yang.zhiwei.tom@vertiv.com <yang.zhiwei.tom@vertiv.com>
2023-03-05 10:37:49 +0800

    加上路由

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
+  <router-link to="/" v-slot="{ href, navigate, isActive }">
+  <li :class="{ 'active': isActive }">
+    <a :href="href" @click="navigate">
+      <span>Home</span>
+    </a>
+  </li>
+</router-link>
+<router-link to="/new" v-slot="{ href, navigate, isActive }">
+  <li :class="{ 'active': isActive }">
+    <a :href="href" @click="navigate">
+      <span>new</span>
+    </a>
+  </li>
+</router-link>
+<router-link to="/design" v-slot="{ href, navigate, isActive }">
+  <li :class="{ 'active': isActive }">
+    <a :href="href" @click="navigate">
+      <span>design</span>
+    </a>
+  </li>
+</router-link>
+
   <router-view />
 </template>
